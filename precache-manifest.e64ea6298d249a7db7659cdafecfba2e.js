d3ce01343fb339c968395461c3ab0bf5ad0f2ea9
AnnaM135 <68158093+AnnaM135@users.noreply.github.com>
2021-11-22 19:27:02 +0400

    Updates

---
+++ precache-manifest.e64ea6298d249a7db7659cdafecfba2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dce399fe7687c3ddd848db237717a8e",
+    "url": "/react_exam/index.html"
+  },
+  {
+    "revision": "53d86cc9e96447bbe15b",
+    "url": "/react_exam/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "53ab4a780824052f9a6e",
+    "url": "/react_exam/static/js/2.457e3dd3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_exam/static/js/2.457e3dd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53d86cc9e96447bbe15b",
+    "url": "/react_exam/static/js/main.b828c33a.chunk.js"
+  },
+  {
+    "revision": "cb2b669a8b5c434d974c",
+    "url": "/react_exam/static/js/runtime-main.c1d73b13.js"
+  }
+]);
